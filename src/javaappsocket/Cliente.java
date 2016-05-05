14d7368207be30ad23f4aa23d3320cf1c5bee2d1
Administrator <root@marcosthomaz.com>
2016-05-05 20:20:07 -0300

    first commit

---
+++ src/javaappsocket/Cliente.java
@@ -0,0 +1,47 @@
+package javaappsocket;
+
+import java.io.IOException; 
+import java.io.PrintStream; 
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+public class Cliente {
+    
+    //IP do servidor
+    static String ipServidor;
+    //porta do servidor
+    static int porta;
+    
+    public Cliente() {
+    
+        //lê o ip e a porta do servidor
+        ipServidor = JOptionPane.showInputDialog(null, "Insira o IP do servidor","127.0.0.1");
+        porta      = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira a porta do servidor","7000"));
+        
+    }
+    
+    public void sendMessage(String msg) {
+        //Declaro o Socket cliente
+        Socket s = null;
+        //Declaro a Stream de saida de dados
+        PrintStream ps = null;
+        try {
+            //Cria o socket com o recurso desejado na porta especificada!
+            s = new Socket(ipServidor, porta);
+            //Cria a Stream de saida de dados
+            ps = new PrintStream(s.getOutputStream());
+            //Imprime uma linha para a stream de saída de dados
+            ps.println(msg);
+        } catch (IOException e) {
+            System.out.println("Algum problema ocorreu ao criar ou enviar dados pelo Socket.");
+            e.printStackTrace();
+        } finally {
+            try {
+                //Encerra o socket cliente
+                s.close();
+            } catch (Exception e) {
+                System.out.println("Algum problema ocorreu ao finalizar o socket");
+            }
+        }
+    }
+}
---
+++ src/javaappsocket/Servidor.java
@@ -0,0 +1,49 @@
+package javaappsocket;
+
+import interfaces.CallBackMessage;
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.net.ServerSocket; 
+import java.net.Socket;
+import javax.swing.JOptionPane;
+
+public class Servidor {
+
+    static int porta;
+    
+    public Servidor(CallBackMessage cbm) {
+        porta = Integer.parseInt(JOptionPane.showInputDialog(null,
+               "Insira a porta em que o servidor vai escutar",
+               "7000"));
+               
+        ServerSocket serv = null;
+        Socket s = null;
+        BufferedReader entrada = null;
+        try {
+            serv = new ServerSocket(porta);
+            System.out.println("Servidor escutando na porta "+porta+"...");
+            s = serv.accept();
+            entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String dados = entrada.readLine();
+            while (dados.indexOf("sair") == -1){
+                if(dados != null){
+                    cbm.appendMessage(dados);
+                }
+                s.close();
+                s = serv.accept();
+                entrada = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                dados = entrada.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                s.close();
+                serv.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
